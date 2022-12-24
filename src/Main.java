3073e94880749b09195d0641a1249f6f47f3a2e8
Ibragim955 <amerhanov_ibragim95@mail.ru>
2022-12-24 20:30:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        var boxerOne = 72.8;
+        var boxerTwo = 82.7;
+        var boxers = boxerOne + boxerTwo;
+        System.out.println("Общий вес " + boxers + "кг! ");
+
+        var boxer = boxerTwo - boxerOne;
+        System.out.println(boxer);
+        var difference = (boxerTwo - boxerOne) % boxers;
+        System.out.println(difference);
+
+        var worker = 640 / 8;
+        System.out.println("Всего работников в компании " + worker + " человек ");
+
+        var workers = worker + 94;
+        System.out.println(workers);
+        var time = 640 / 174;
+        System.out.println("Если в компании работает " + workers + " человек,то всего "  + time + " часов работы может быть поделено между сотрудниками");
+
+    }
+}
